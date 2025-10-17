851f7c429a10a8377e104c436690027550188c99
SungJiYong <sungjy2020@gmail.com>
2025-10-17 22:25:23 +0900

    feat: 업데이트 반영 (app/main.py 기준 실행/설정)

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\buddd\OneDrive\Apps\TweezeAI\.venv
+set VIRTUAL_ENV=C:\dev\TweezeAI\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
